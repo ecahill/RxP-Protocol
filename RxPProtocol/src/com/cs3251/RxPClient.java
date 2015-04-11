03f4c2e1daf8d8bac06f7be9d0164709d1497f0f
Emily Cahill <c.emily@rocketmail.com>
2015-04-11 00:43:09 -0400

    Added to main method of client

--- RxPProtocol/src/com/cs3251/RxPClient.java
+++ RxPProtocol/src/com/cs3251/RxPClient.java
@@ -1,13 +1,20 @@
 package com.cs3251;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class RxPClient implements RxPClientInterface {
-	
+	private static int socketPort;
+	private static InetAddress ipAddress;
+	private static int portNum;
 	
 	
 	public void startClientServer(){
-		//must bind to even number socket
+		if ((socketPort&1)==1){
+			System.out.println("Socket must bind to even port number. Exiting.");
+			System.exit(0);
+		}
 	}
 	public void connectToServer(){
 		
@@ -29,8 +36,30 @@ public class RxPClient implements RxPClientInterface {
 	}
 	
 	public static void main(String[] args){
+		RxPClient c = new RxPClient();
 		if (args.length!=4||!args[0].equals("FxA-client")){
 			System.out.println("The arguments entered were invalid. Exiting.");
+			System.exit(0);
+		}
+		try{
+			socketPort = Integer.parseInt(args[1]);
+		}
+		catch(NumberFormatException ex){
+			System.out.println("Port number must be an int. Exiting.");
+			System.exit(0);
+		}
+		try{
+			ipAddress = InetAddress.getByName(args[2]);
+		}
+		catch(UnknownHostException ex){
+			System.out.println("IP Address was invalid. Exiting.");
+			System.exit(0);
+		}
+		try{
+			portNum = Integer.parseInt(args[3]);
+		}
+		catch(NumberFormatException ex){
+			System.out.println("Port number was invalid. Exiting.");
 			System.exit(0);
 		}
 		
